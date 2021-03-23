ad3c9bb5df85ba7d7b0eddfb15b67b6dedafc1c0
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2021-03-23 14:49:12 +0900

    latest

--- .github/scripts/report-annotations.js
+++ .github/scripts/report-annotations.js
@@ -9,15 +9,12 @@ module.exports = ({github, context}) => {
   const run_id = context.runId
   const pull_request = context.payload.pull_request
 
-  collectAnnotations().then(annotations => {
-    commentOnPullRequest(
-      formatAnnotations(annotations)
-    )
+  collectAnnotationWarnings().then(data => {
+    console.log(data)
+    commentOnPullRequest(formatAnnotations(data))
   })
 
-  ///
-
-  function collectAnnotations() {
+  function collectAnnotationWarnings() {
     return github.actions.getWorkflowRun({
       owner,
       repo,
@@ -31,6 +28,7 @@ module.exports = ({github, context}) => {
         return Promise.all(
           check_runs
             .filter(check_run => check_run.output.annotations_count > 0)
+            .filter(check_run => check_run.conclusion === 'success')
             .map(check_run => {
               return github.checks.listAnnotations({
                 owner,
@@ -51,18 +49,23 @@ module.exports = ({github, context}) => {
     })
   }
 
-  function formatAnnotations(annotations) {
-    if (annotations.length === 0) {
+  function formatAnnotations(data) {
+    if (data.length === 0) {
       return
     }
 
-    let run = annotations[0].run // TODO: rethink structure
+    const run = data[0].run
 
-    let out = "<h2>Annotations</h2>"
-    out += `<em>Below is a list of annotations from the latest <a href="${run.html_url}">GitHub Action run</a></em>`
-    annotations.forEach(a => {
+    let out = "<h2>GitHub Actions Warnings</h2>"
+    out += `<em>
+              Below is a list of annotations from successful jobs in the latest <a href="${run.html_url}">GitHub Action run</a>. 
+              They may incidate warnings or flapping tests that should be addressed.
+            </em>`
+    out += `<ul>`
+    data.forEach(a => {
       out += `<li><strong>${a.check_run.output.title}:</strong> ${a.annotation.message}</li>`
     })
+    out += `</ul>`
     return out
   }
 
@@ -79,5 +82,3 @@ module.exports = ({github, context}) => {
     })
   }
 }
-
-//dummy comment
