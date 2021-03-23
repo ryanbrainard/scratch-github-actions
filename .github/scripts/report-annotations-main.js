c235ead45dd3d73738e18865e96dd9c0c603c8f9
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2021-03-23 12:31:37 +0900

    simulate accessor

--- .github/scripts/report-annotations-main.js
+++ .github/scripts/report-annotations-main.js
@@ -141,22 +141,10 @@ const context = {
   runId: 678250182
 }
 
-context.repo = function() {
-  if (process.env.GITHUB_REPOSITORY) {
-    const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
-    return {owner, repo}
-  }
-
-  if (this.payload.repository) {
-    return {
-      owner: this.payload.repository.owner.login,
-      repo: this.payload.repository.name
-    }
-  }
-
-  throw new Error(
-    "context.repo requires a GITHUB_REPOSITORY environment variable like 'owner/repo'"
-  )
+// simulate accessor
+context.repo = {
+    owner: context.payload.repository.owner.login,
+    repo: context.payload.repository.name
 }
 
 script({github, context})
